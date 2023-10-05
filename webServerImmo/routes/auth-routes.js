9122de98b8a3e54221084db01d495b729c592b34
aa195113 <abderrahmane.abaakil@etu.univ-lehavre.fr>
2023-10-05 20:33:50 +0200

    Add the route for the registration page

--- webServerImmo/routes/auth-routes.js
+++ webServerImmo/routes/auth-routes.js
@@ -6,4 +6,8 @@ router.get('/login', async function(req, res, next) {
     res.render('login',{ title: 'Login'});
 });
 
+router.get('/signup', function(req, res){
+    res.render('register',{message: Register});
+});
+
 module.exports = router;
