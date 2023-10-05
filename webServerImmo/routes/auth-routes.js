0fdaf911814a0d9b77b77b6af0b976a95f87c48d
aa195113 <abderrahmane.abaakil@etu.univ-lehavre.fr>
2023-10-05 20:47:58 +0200

    add the rout for logging out

--- webServerImmo/routes/auth-routes.js
+++ webServerImmo/routes/auth-routes.js
@@ -6,6 +6,10 @@ router.get('/login', async function(req, res, next) {
     res.render('login',{ title: 'Login'});
 });
 
+router.get('/logout',async function(req, res, next) {
+    res.send('logging out');
+});
+
 router.get('/google',async function(req, res, next) {
     res.send('logging with google');
 });
