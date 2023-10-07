162cb74f58cd88fd306e72187dc525e7fb937ec2
Mehdi Khaddor <el-mehdi.khaddor@etu.univ-lehavre.fr>
2023-10-07 18:23:40 +0200

    Added Update announecement Func

--- webServerImmo/routes/annonces.js
+++ webServerImmo/routes/annonces.js
@@ -86,4 +86,74 @@ router.post('/create-announcement', async function (req, res, next) {
     }
 });
 
+// Route to render the edit announcement page
+router.get('/edit-announcement/:id', authCheck,async (req, res) => {
+    const announcementId = req.params.id;
+
+    try {
+        // Find the announcement by ID
+        const announcement = await Annonce.findById(announcementId);
+        // Render a template to edit announcement details
+        res.render('edit-announcement', { announcement:announcement, user:req.user });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while fetching the announcement for editing.');
+    }
+});
+
+// Route to handle edit-announcement
+router.post('/edit-announcement/:id', async (req, res) => {
+    const announcementId = req.params.id;
+    const updatedAnnouncementData = req.body;
+
+    try {
+        const announcement = await Annonce.findById(announcementId);
+        if (!announcement) {
+            return res.status(404).send('Announcement not found');
+        }
+
+        // Update
+        announcement.titre = updatedAnnouncementData.titre;
+        announcement.type = updatedAnnouncementData.type;
+        announcement.status = updatedAnnouncementData.status;
+        announcement.description = updatedAnnouncementData.description;
+        announcement.prix = updatedAnnouncementData.prix;
+        announcement.publication = updatedAnnouncementData.publication === 'on';
+
+        // Handle photos
+        if (req.files && req.files.photos) {
+            const imageDossier = __dirname + '/../public/images/';
+            const photoSrc = [];
+
+            if (!Array.isArray(req.files.photos)) {
+                req.files.photos = [req.files.photos];
+            }
+
+            req.files.photos.forEach((p, i) => {
+                const splittedName = p.name.split('.');
+                const imgUrl =
+                    new Date().getTime() +
+                    i +
+                    '.' +
+                    splittedName[splittedName.length - 1];
+                p.mv(path.join(imageDossier, imgUrl), (err) => {
+                    if (err) {
+                        console.error(err);
+                        // Handle the error here
+                    }
+                });
+                photoSrc.push(imgUrl);
+            });
+            announcement.photos = photoSrc;        }
+        // Save the updated announcement
+        await announcement.save();
+
+        // Redirect back to the announcements page after editing
+        res.redirect('/ad/annonces');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(`An error occurred while editing the announcement: ${error.message}`);
+    }
+});
+
 module.exports=router;
