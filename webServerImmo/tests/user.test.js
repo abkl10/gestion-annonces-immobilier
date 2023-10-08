2f87b14642ad67512a1e87da696ac13f07fb8374
Mehdi Khaddor <el-mehdi.khaddor@etu.univ-lehavre.fr>
2023-10-08 21:48:46 +0200

    Added Some test to Anounecement route

--- webServerImmo/tests/user.test.js
+++ webServerImmo/tests/user.test.js
@@ -2,7 +2,6 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app'); // Import your Express app
 const passportStub = require('passport-stub'); // Passport-stub for testing
-var express = require('express');
 const mongoose = require('mongoose');
 
 chai.use(chaiHttp);
@@ -11,13 +10,39 @@ const expect = chai.expect;
 // Initialize passportStub to work with passport
 passportStub.install(app);
 
+// Define the MongoDB test database URL
+const mongoTestUrl = 'mongodb://localhost:27017/'; // Replace with your test database URL
+
 describe('Authentication Routes', () => {
-  before(() => {
-    passportStub.login({ username: 'test', password: 'test' }); // Simulate user login
+  // Connect to the test database before running tests
+  before(async () => {
+    try {
+      // Connect to the MongoDB test database
+      await mongoose.connect(mongoTestUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log('Connected to the MongoDB test database.');
+    } catch (error) {
+      console.error('Error connecting to the MongoDB test database:', error);
+    }
+    
+    // Simulate user login
+    passportStub.login({ username: 'admin', password: 'admin' });
   });
 
-  after(() => {
-    passportStub.logout(); // Simulate user logout
+  // Disconnect from the test database after running tests
+  after(async () => {
+    try {
+      // Disconnect from the MongoDB test database
+      await mongoose.disconnect();
+      console.log('Disconnected from the MongoDB test database.');
+    } catch (error) {
+      console.error('Error disconnecting from the MongoDB test database:', error);
+    }
+    
+    // Simulate user logout
+    passportStub.logout();
   });
 
   it('should render login page', (done) => {
@@ -30,7 +55,18 @@ describe('Authentication Routes', () => {
         done();
       });
   });
+  // it('should successfully log in with valid credentials', async () => {
+  //   const res = await chai
+  //     .request(app)
+  //     .post('/auth/login')
+  //     .send({ username: 'test', password: 'test' });
   
+  //   expect(res).to.redirect; // Ensure a redirect
+  //   expect(res).to.redirectTo('/'); // Ensure a redirect to the home page
+  // });
+  
+  
+
   it('should render signup page', (done) => {
     chai
       .request(app)
@@ -41,8 +77,6 @@ describe('Authentication Routes', () => {
         done();
       });
   });
-
-
 
   // Add more tests for other routes as needed
 });
