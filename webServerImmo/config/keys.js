1f107010af97510ed3c3ab04cdecb66eb92c3ad7
ABAAKIL Abderrahman <564-aa195113@users.noreply.www-apps.univ-lehavre.fr>
2023-10-09 07:44:30 +0000

    Update keys.js

--- webServerImmo/config/keys.js
+++ webServerImmo/config/keys.js
@@ -1,8 +1,8 @@
 // Fill the required fields with you own credentials
 module.exports={
     google:{
-        clientId:"996796681853-9h6fa3iestls5rp1dnch1p3o0bpafn9o.apps.googleusercontent.com",
-        clientSecret:"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"
+        clientId:"*****************************",
+        clientSecret:"*********************************"
     },
     session:{
         cookieKey:'abklisalegend'
